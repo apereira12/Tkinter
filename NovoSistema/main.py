e384e2b80f3811ae8929ed1e8834a35aaa022630
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-12-12 01:24:03 -0300

    Add files via upload

--- NovoSistema/main.py
+++ NovoSistema/main.py
@@ -2,14 +2,15 @@ import customtkinter
 import tkinter as tk
 import os
 from PIL import Image
+import pandas as pd
+from tkinter import ttk
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
-        self.title("Campeonato Brasileiro.py")
-        self.geometry("850x730")
+        self.title("Campeonato Brasileiro")
+        self.geometry("800x1200")
 
         # set grid layout 1x2
         self.grid_rowconfigure(0, weight=1)
@@ -65,12 +66,13 @@ class App(customtkinter.CTk):
                                                                 command=self.change_appearance_mode_event)
         self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
 
+        self.create_home_frame()
+        self.create_second_frame()
+        self.create_third_frame()
+        # select default frame
+        self.select_frame_by_name("home")
 
-
-
-
-
-        # create home frame
+    def create_home_frame(self):
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
@@ -84,39 +86,96 @@ class App(customtkinter.CTk):
         self.textboxHome = customtkinter.CTkTextbox(self.home_frame)
         self.textboxHome.grid(row=1, column=0, padx=(10, 0), pady=(10, 0))
         self.textboxHome.insert("0.0", "Seja Bem-vindo a \n "
-                                       "\n Análise do Brasileirão", customtkinter.CENTER)
+                                           "\n Análise d"
+                                           "o Brasileirão", customtkinter.CENTER)
 
-        # create second frame
-
+    def create_second_frame(self):
         self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.second_frame.grid_columnconfigure(0, weight=1)
+        self.filtros1 = customtkinter.CTkFrame(self.second_frame)
+        self.filtros1.grid(row=0, column=0, sticky="nsew")
+        self.filtros1.grid_rowconfigure(7, weight=4)
 
-        self.filterYear = customtkinter.CTkOptionMenu(self.second_frame,
-                                                               values=["Light", "Dark", "System"],
-                                                               command=self.change_appearance_mode_event)
-        self.filterYear.grid(row=0, column=2, padx=(10, 0), pady=(10, 0))
+        self.anos_disponiveis = []
 
-        self.tree = tk.ttk.Treeview(self.second_frame)
-        columns = ("Índice", "NumeroJogo", "Data", "Hora", "Estadio", "Cidade")
-        self.tree["columns"] = columns
+        self.tree1 = ttk.Treeview(self.second_frame, show="headings")
+        self.tree1.grid(row=3, column=0, columnspan=3, pady=10, sticky="nsew")
 
+
+
+        # Adiciona dados do Excel à Treeview
+        self.jogos("CampeonatoBrasileiro_2018-2023_SerieA.xlsx", sheet_name="Jogos")
+
+        scrollbar = ttk.Scrollbar(self.second_frame, orient="vertical", command=self.tree1.yview)
+        scrollbar.grid(row=3, column=3, sticky="ns")
+        self.tree1.configure(yscrollcommand=scrollbar.set)
+
+        scrollbar_x = ttk.Scrollbar(self.second_frame, orient="horizontal", command=self.tree1.xview)
+        scrollbar_x.grid(row=4, column=0, columnspan=3, sticky="ew")
+        self.tree1.configure(xscrollcommand=scrollbar_x.set)
+
+    def jogos(self, arquivo_excel, sheet_name):
+        # Lê os dados do Excel usando pandas
+        df = pd.read_excel(arquivo_excel, sheet_name)
 
         # Configuração das colunas
-        self.tree.heading("#0", text="Índice")
-        self.tree.column("#0", anchor=tk.W, width=50)
-        self.tree.tag_configure("all_rows", background="lightblue")
+        self.tree1["columns"] = df.columns.tolist()
 
-        for col in columns[1:]:
-            self.tree.heading(col, text=col)
-            self.tree.column(col, anchor=tk.W, width=150)  # Ajuste a largura conforme necessário
+        for col in df.columns:
+            self.tree1.heading(col, text=col)
+            self.tree1.column(col, anchor=tk.W, width=150)
 
-        self.tree.grid(row=2, column=0, columnspan=3, pady=10)
+        # Itera sobre as linhas do DataFrame e insere os dados na Treeview
+        for i, row in df.iterrows():
+            values = [str(row[col]) for col in df.columns]
+            self.tree1.insert("", "end", values=values)
 
-        # create third frame
+        # Cria a StringVar no escopo do frame
+        self.anos_disponiveis = sorted(df['AnoJogo'].unique())
+        self.anos_disponiveis = ["Todos"] + [str(ano) for ano in self.anos_disponiveis]
+
+        # Cria um novo CTkOptionMenu com as novas opções
+        self.filtro_var = tk.StringVar(master=self.filtros1)
+        self.filtro_var.set("Todos")  # Valor padrão
+
+        # Opção do filtro
+        self.option_menu = tk.OptionMenu(self.filtros1, self.filtro_var, *self.anos_disponiveis)
+        self.option_menu.grid(row=0, column=2, padx=(10, 0), pady=(10, 0))
+
+        # Associar a função filtrar_dados à variável de controle
+        self.filtro_var.trace_add("write", self.filtrar_dados)
+
+
+        self.df = df  # Atribui o DataFrame à variável de instância
+
+        self.atualizar_dados()  # Chama a função para atualizar os dados
+
+    def filtrar_dados(self, *args):
+        filtro = self.filtro_var.get()
+
+        # Limpar a Treeview
+        self.tree1.delete(*self.tree1.get_children())
+
+        # Adicionar de volta apenas os itens que correspondem ao filtro
+        for i, row in self.df.iterrows():
+            if filtro == "Todos" or filtro.lower() == str(row['AnoJogo']).lower():
+                values = [str(row[col]) for col in self.df.columns]
+                self.tree1.insert("", "end", values=values)
+
+    def atualizar_dados(self):
+        # Limpar a Treeview
+        self.tree1.delete(*self.tree1.get_children())
+
+        # Itera sobre as linhas do DataFrame e insere os dados na Treeview
+        for i, row in self.df.iterrows():
+            values = [str(row[col]) for col in self.df.columns]
+            self.tree1.insert("", "end", values=values)
+
+    def create_third_frame(self):
         self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
 
-        # select default frame
-        self.select_frame_by_name("home")
+
+
 
     def select_frame_by_name(self, name):
         # set button color for selected button
@@ -149,6 +208,8 @@ class App(customtkinter.CTk):
 
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
+
+
 
 
 if __name__ == "__main__":
